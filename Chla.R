ac8daa7372642d2ed53d258ebc1f8a73b99c7712
juliekelso <julia.kelso@gmail.com>
2016-01-29 11:55:56 -0700

    Init commit

--- Chla.R
+++ Chla.R
@@ -1,3 +1,7 @@
 getwd()
 
-read.csv("C:/Users/Julie Kelso/Desktop/Box Sync/GAMUT_Sampling/CSVs/LR_Datasheet")
+read.csv("C:/Users/Julie Kelso/Dropbox/LR_Datasheet")
+
+LRDatasheet = read.csv("C:/Users/Julie Kelso/Desktop/Box Sync/GAMUT_Sampling/CSVs/LR_Datasheet.csv")
+
+Chla = read.csv("C:/Users/Julie Kelso/Dropbox/iUtah/biweekly sampling/Chla.csv")
