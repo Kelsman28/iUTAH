f554301c4c1961f7449f53e22635775cf8c047a4
juliekelso <julia.kelso@gmail.com>
2016-02-01 10:06:00 -0700

    init commit 160201

--- Chla.R
+++ Chla.R
@@ -5,17 +5,16 @@ read.csv("C:/Users/Julie Kelso/Dropbox/LR_Datasheet")
 
 LRDatasheet = read.csv("C:/Users/Julie Kelso/Desktop/Box Sync/GAMUT_Sampling/CSVs/LR_Datasheet.csv")
 
-Chla = read.csv("C:/Users/Julie Kelso/Dropbox/iUtah/biweekly sampling/Chl a/Chla.csv")
+Chla = read.csv("C:/Users/Julie Kelso/Dropbox/iUtah/biweekly sampling/Chl a/Chla.csv", stringsAsFactors = FALSE)
 
 Chla = Chla[-c(414:1048521),]
-
-### convert data values to numeric  ####
-Chla$DataValue <- as.numeric(Chla$DataValue) 
 
 ### remov outliers  ###
 Chla$DataValue[Chla$DataValue > 20 ] = "NA"
 
 ValChla = merge(LRDatasheet, Chla, by=c("Date", "SiteCode"), all.y = TRUE)
+
+ValChla <- rowMeans(ValChla[, c(14)])
 
 #average those rows that have multiple values 
 
