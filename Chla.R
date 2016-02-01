cd3e8aa616579560dfa1c34c3b09d3d70a4a6d76
juliekelso <julia.kelso@gmail.com>
2016-02-01 10:28:55 -0700

    Reorderd ValChla now must reorder Chla to try and figure out why ValCHla has missing datavalues

--- Chla.R
+++ Chla.R
@@ -8,12 +8,19 @@ LRDatasheet = read.csv("C:/Users/Julie Kelso/Desktop/Box Sync/GAMUT_Sampling/CSV
 Chla = read.csv("C:/Users/Julie Kelso/Dropbox/iUtah/biweekly sampling/Chl a/Chla.csv", stringsAsFactors = FALSE)
 
 Chla = Chla[-c(414:1048521),]
+Chla = Chla[order(Chla$Watershed, Chla$LocalDateTime),]
 
 ### remov outliers  ###
 Chla$DataValue[Chla$DataValue > 20 ] = "NA"
 
 ValChla = merge(LRDatasheet, Chla, by=c("Date", "SiteCode"), all.y = TRUE)
 
+### sort byt watershed, Date
+ValChla = ValChla[order(ValChla$Watershed, ValChla$LocalDateTime), ]  
+#### reorder columns ####
+ValChla = ValChla[,c(1,2,14,3:35)]
+
+df[,c(1,2,3,4,5:50)]
 ValChla <- rowMeans(ValChla[, c(14)])
 
 #average those rows that have multiple values 
