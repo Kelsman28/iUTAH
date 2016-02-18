563aa0e1947e56a8c7ede98fdeb40a76f6931f77
juliekelso <julia.kelso@gmail.com>
2016-02-18 14:05:06 -0700

    replaced dates for HWY 252 to 140913

--- iUtahRespositroy.R
+++ iUtahRespositroy.R
@@ -11,17 +11,17 @@ library(data.table)
 library(stats)
 library(tidyr)
 
-setwd("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository")
+
 
 SepCN = read.csv("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository/SepCN.csv")
 SepH = read.csv("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository/SepH.csv")
 
 colnames(SepH)[c(1,9,10,14)] <- c("SampleID","dD", "PerH", "NumericDate")
+
+### reduce SepH to columns listed in myvars
 myvars <- c("SampleID" , "dD","PerH","Comments","Sample.ID","Description",
             "NumericDate","Month","Watershed","Site.ID","TrayName")
 SepH <- SepH[myvars]
 
-SepH$NumericDate = replace(SepH$NumericDate, 
-SepCN$NumericDate[SepCN$Site.ID == "LR_HWY252"] <- "140913"
-LR_HWY252  should be 140913 but is 140915
-LR_HWY252
+#LR_HWY252  should be 140913 but is 140915
+SepCN$NumericDate[SepCN$Reach == "LR_HWY252"] <- "140913"
