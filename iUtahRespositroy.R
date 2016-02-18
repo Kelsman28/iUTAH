d4fc8d2ef5cea74d84c9eea1eadf221dcea1b336
juliekelso <julia.kelso@gmail.com>
2016-02-18 14:32:59 -0700

    changed all dates to match "All site with Dates" datasheet

--- iUtahRespositroy.R
+++ iUtahRespositroy.R
@@ -13,8 +13,8 @@ library(tidyr)
 
 
 
-SepCN = read.csv("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository/SepCN.csv")
-SepH = read.csv("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository/SepH.csv")
+SepCN = read.csv("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository/SepCN.csv", stringsAsFactors = FALSE)
+SepH = read.csv("C:/Users/Julie Kelso/Dropbox/All Isotopes/iUtahRespository/SepH.csv", stringsAsFactors = FALSE)
 
 colnames(SepH)[c(1,9,10,14)] <- c("SampleID","dD", "PerH", "NumericDate")
 
@@ -23,5 +23,22 @@ myvars <- c("SampleID" , "dD","PerH","Comments","Sample.ID","Description",
             "NumericDate","Month","Watershed","Site.ID","TrayName")
 SepH <- SepH[myvars]
 
+SepH = SepH[-c(63:1048294),]
+
 #LR_HWY252  should be 140913 but is 140915
-SepCN$NumericDate[SepCN$Reach == "LR_HWY252"] <- "140913"
+SepCN$Date[SepCN$Reach == "LR_HWY252"] <- "140913"
+SepCN$Date[SepCN$Reach == "LR_MainSt_BA"] <- "140911"
+SepCN$Date[SepCN$Reach == "LR_TonyGrove_BA"] <- "140904"
+SepCN$Date[SepCN$Reach == "PR_CanyonView" ] <- "140903"
+SepCN$Date[SepCN$Reach == "PR_CH_AA"] <- "140903"
+SepCN$Date[SepCN$Reach == "PR_LM_BA"] <- "140903"
+
+### for all RB sites
+df$question[df$variable %in% c("var2", "var3", "var4")] <- "text"
+d
+SepCN$Date[SepCN$Reach %in% c("RB_1300E","RB_1300S","RB_ARBR_AA","RB_CG_BA","RB_FD_AA" , 
+                              "RB_KF_BA" ,"RB_RBG_BA","RB_TM")] = "140901"
+
+
+
+           
